43112b09fe8d149b44d40a21fb14df67f11e3cae
Ahmet <bozyurtahmet@gmail.com>
2022-02-14 19:03:56 +0300

    Design product row layout

---
+++ app/src/main/java/com/example/makeup/models/ProductColor.kt
@@ -0,0 +1,11 @@
+package com.example.makeup.models
+
+
+import com.google.gson.annotations.SerializedName
+
+data class ProductColor(
+    @SerializedName("colour_name")
+    val colourName: String,
+    @SerializedName("hex_value")
+    val hexValue: String
+)
---
+++ app/src/main/java/com/example/makeup/models/Products.kt
@@ -0,0 +1,6 @@
+package com.example.makeup.models
+
+
+import com.google.gson.annotations.SerializedName
+
+class Products : ArrayList<ProductsItem>()
