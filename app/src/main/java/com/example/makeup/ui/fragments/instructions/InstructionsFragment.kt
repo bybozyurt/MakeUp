4b1da6c9619d0da15da30a00bb04601aed6bd861
Ahmet <bozyurtahmet@gmail.com>
2022-02-20 21:06:43 +0300

    InstructionsFragment done.

--- app/src/main/java/com/example/makeup/ui/fragments/instructions/InstructionsFragment.kt
+++ app/src/main/java/com/example/makeup/ui/fragments/instructions/InstructionsFragment.kt
@@ -5,16 +5,47 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebViewClient
 import com.example.makeup.R
+import com.example.makeup.databinding.FragmentInstructionsBinding
+import com.example.makeup.models.ProductsItem
+import com.example.makeup.util.Constants.Companion.PRODUCTS_BUNDLE_KEY
 
 class InstructionsFragment : Fragment() {
+
+    private var _binding: FragmentInstructionsBinding? = null
+    private val binding get() = _binding!!
+    private var myBundle: ProductsItem? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_instructions, container, false)
+        _binding = FragmentInstructionsBinding.inflate(inflater, container, false)
+        initBundle()
+        initWebView()
+
+
+        return binding.root
+    }
+
+    private fun initWebView(){
+        with(binding.instructionsWebView){
+            webViewClient = object : WebViewClient() { }
+            val websiteUrl: String = myBundle!!.productLink.toString()
+            loadUrl(websiteUrl)
+        }
+    }
+
+    private fun initBundle(){
+        val args = arguments
+        myBundle = args?.getParcelable(PRODUCTS_BUNDLE_KEY)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 
