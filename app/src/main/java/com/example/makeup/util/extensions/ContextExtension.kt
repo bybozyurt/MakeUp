221dc416eac9e6ccffcb8ca46b4c29c2bc117ff9
Ahmet <bozyurtahmet@gmail.com>
2022-03-29 16:49:41 +0300

    SnackBar Dark Theme Added.

--- app/src/main/java/com/example/makeup/util/extensions/ContextExtension.kt
+++ app/src/main/java/com/example/makeup/util/extensions/ContextExtension.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.annotation.StringRes
+import androidx.core.content.ContextCompat
 import com.example.makeup.R
 import com.example.makeup.databinding.CustomSnackbarBinding
 import com.example.makeup.util.Constants
@@ -23,14 +24,19 @@ fun Context.showCustomSnackBar(message: String, container: View?, icon: String) 
         val snackView = View.inflate(this, R.layout.custom_snackbar, null)
         val binding = CustomSnackbarBinding.bind(snackView)
         val snackBar = Snackbar.make(container, "", Snackbar.LENGTH_LONG)
+        snackBar.setBackgroundTint(
+            ContextCompat.getColor(
+                this.applicationContext,
+                R.color.snackBarBackgroundColor
+            )
+        )
         snackBar.apply {
             (view as ViewGroup).addView(binding.root)
             binding.tvMessage.text = message
             if (icon == Constants.SAVE_ICON) {
                 binding.imgDelete.hide()
                 binding.imgSave.show()
-            }
-            else if (icon == Constants.DELETE_ICON) {
+            } else if (icon == Constants.DELETE_ICON) {
                 binding.imgSave.hide()
                 binding.imgDelete.show()
             }
